f77d51193aafbd15214b62d108806a26ba0c626d
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-12 14:03:30 +0900

    mod: queryClient, router 객체 생성 및 전달
    main.tsx
    - BrowserRouter 인스턴스 생성 및 RouterProvider에게 전달
    - rootLoader 연결 및 rootLoader에게 queryClient 객체 전달
    - React Query의 QueryClient 인스턴스 생성 및 QueryClientProvider
      에게 QueryClient 전달

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root, { loader as rootLoader } from './routes/Root';
 import './index.css';
 
 // Start the mocking conditionally.
@@ -9,8 +11,20 @@ if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
 
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    loader: rootLoader(queryClient),
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
